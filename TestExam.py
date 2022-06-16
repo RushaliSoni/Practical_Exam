d3412f70e5eccc7f39991ee3ba5e12e3b999d917
RushaliSoni <rushalilangaliya2@gmail.com>
2022-06-16 13:51:50 +0530

    Read Data from json File

--- TestExam.py
+++ TestExam.py
@@ -1,8 +1,9 @@
 import json
 
-with open('test.json','r') as file:
-    json_data = json.load(file)
+file = open('test.json','r')
+data = json.load(file)
+print(data)
 
-for info in json_data:
-    print(info)
+
+
 
